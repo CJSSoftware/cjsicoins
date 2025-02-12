a907ba0ed5bed0e1fe3887ee5ff9c29887ec06af
CJS Software Development <connorstreets1@hotmail.com>
2025-02-12 07:14:52 +0000

    Update Footer.js

--- frontend/src/componants/Footer.js
+++ frontend/src/componants/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
-                <p>© {new Date().getFullYear()} CJSGaming LTD. All rights reserved.</p>
+                <p>© {new Date().getFullYear()} CJSGaming LTD. ®️ All rights reserved.</p>
                 <Link to="https://hostengland.co.uk/privacy-policy" className="footer-link">Privacy Policy</Link>
             </div>
         </footer>
